b886f3180b9f165cd235ab5391f7778f999285ea
JacobVestergaard <119966598+JacobVestergaard@users.noreply.github.com>
2026-04-17 14:02:57 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'flashcards-v1';
+const FILES = ['/', '/index.html'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => { const clone = res.clone(); caches.open(CACHE).then(c => c.put(e.request, clone)); return res; }))));
