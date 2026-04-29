aca8cb1ecbd69abebfe7fa33521949c74c764d02
JacobVestergaard <jacobhv@live.dk>
2026-04-29 10:07:48 +0200

    sync 2026-04-29 10:07

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flashcards-v1';
-const FILES = ['/', '/index.html'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => { const clone = res.clone(); caches.open(CACHE).then(c => c.put(e.request, clone)); return res; }))));
+const C='fc-v4',F=['./','./index.html'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
