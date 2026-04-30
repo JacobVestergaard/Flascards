4b865c0b00b3de57ae71507695e68585c9747fe6
JacobVestergaard <jacobhv@live.dk>
2026-04-30 09:47:00 +0200

    sync 2026-04-30 09:47

--- sw.js
+++ sw.js
@@ -1,19 +1,7 @@
-const C='fc-v6',F=['./','./index.html'];
+const C='fc-v6',F=['./','./index.html','./decks.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
-  const url = new URL(e.request.url);
-  if(url.pathname.endsWith('.json')) {
-     e.respondWith(
-       caches.match(e.request).then(cached => {
-         return cached || fetch(e.request).then(res=>{
-             const rc = res.clone();
-             caches.open(C).then(c=>c.put(e.request, rc));
-             return res;
-         });
-       })
-     );
-     return;
-  }
+  // Kortfiler hentes altid frisk (cache-buster via ?v=TS håndterer det)
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
