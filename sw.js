bf7f13e10a3559ffd37099d968a1d0d25eae1a8d
JacobVestergaard <jacobhv@live.dk>
2026-05-05 11:48:18 +0200

    sync 2026-05-05 11:48

--- sw.js
+++ sw.js
@@ -1,7 +1,1 @@
-const C='fc-v6',F=['./','./index.html','./decks.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  // Kortfiler hentes altid frisk (cache-buster via ?v=TS håndterer det)
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+const V='fc8';self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(['./','./index.html','./decks.json'])).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
